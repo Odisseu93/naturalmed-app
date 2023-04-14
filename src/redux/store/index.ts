05a727fffead4b4705b7ca56dae48411f6d17039
Ulisses <ulissessuporteti01@gmail.com>
2023-04-14 19:45:11 -0300

    chore(store): remove unused code

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -5,14 +5,3 @@ import offersSlice, { add, update, remove } from './slices/offersSlice';
 export const store = configureStore({
 	reducer: offersSlice.reducer,
 });
-
-store.subscribe(() => console.log(store.getState()));
-
-store.dispatch(add({
-	productName: 'apple',
-	type: 'default',
-	category: 'food',
-	description: 'blah blah blah...',
-	price: 10.50,
-	image: 'sdgdsgsdgs',
-	isActive: false,}));
