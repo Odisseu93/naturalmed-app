624dc94fa5e0b83663329d22c005609c7122e92c
Ulisses <ulissessuporteti01@gmail.com>
2023-04-14 08:56:02 -0300

    fix: style and HTML of the "HighlightOffer" and "ButtonBuy"

--- src/components/HighlightOffer/index.tsx
+++ src/components/HighlightOffer/index.tsx
@@ -7,15 +7,19 @@ import  ButtonBuy from '@/components/ButtonBuy/index';
 const HighlightOffer: React.FC<IOffers> = ({id, productName, image, description, price}: IOffers) => {
 	return (
 		<section className={styles.wrapper} id={id}>
-			<h2 className={`${styles.title} ${fonts.roboto500.className}`}>{productName}</h2>
-			<img
-				src={image}
-				className={styles.image}
-				alt="product image"
-			/>
-			<p className={`${styles.description} ${fonts.roboto400.className}`}>{description}</p>
-			<strong className={`${styles.price}  ${fonts.roboto700.className}`}>{`$${price.toFixed(2)}`}</strong>
-			<ButtonBuy key={String(uuidv4())} text='Buy' />
+			<div className={styles.box1}>
+				<h2 className={`${styles.title} ${fonts.roboto500.className}`}>{productName}</h2>
+				<img
+					src={image}
+					className={styles.image}
+					alt="product image"
+				/>
+			</div>
+			<div className={styles.box2}>
+				<strong className={`${styles.price}  ${fonts.roboto700.className}`}>{`$${price.toFixed(2)}`}</strong>
+				<p className={`${styles.description} ${fonts.roboto400.className}`}>{description}</p>
+				<ButtonBuy key={String(uuidv4())} text='Buy' />
+			</div>
 		</section>
 	);
 };
