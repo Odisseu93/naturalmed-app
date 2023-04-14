5c59999036e552283849a331aaccdac9aa6871fe
Ulisses <ulissessuporteti01@gmail.com>
2023-04-14 19:43:53 -0300

    chore(offersSlice): change  initialState add data to test ui

--- src/redux/store/slices/offersSlice/index.ts
+++ src/redux/store/slices/offersSlice/index.ts
@@ -15,14 +15,112 @@ export interface IOffers {
 
 const initialState: IOffers[] = [
 	{
-		id: '',
-		productName: '',
-		type: '',
-		category: '',
-		description: '',
-		price: 0,
-		image: '',
+		id: uuidv4(),
+		productName: 'Lorem ipsum dolor sit amet.',
+		type: 'carousel',
+		category: 'Healty & Care',
+		description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores non quasi provident. Fugit ratione nostrum praesentium, doloremque qui nobis illo!',
+		price: 9.5,
+		image:
+      'https://images.unsplash.com/photo-1512069772995-ec65ed45afd6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG5hdHVyYWwlMjByZW1lZHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+		isActive: true,
+	},
+	{
+		id: uuidv4(),
+		productName: 'Lorem ipsum dolor sit amet.',
+		type: 'carousel',
+		category: 'Healty & Care',
+		description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores non quasi provident. Fugit ratione nostrum praesentium, doloremque qui nobis illo!',
+		price: 18.9,
+		image:
+      'https://images.unsplash.com/photo-1624454002302-36b824d7bd0a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fG5hdHVyYWwlMjByZW1lZHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+		isActive: true,
+	},
+	{
+		id: uuidv4(),
+		productName: 'Lorem ipsum dolor sit amet.',
+		type: 'carousel',
+		category: 'Healty & Care',
+		description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores non quasi provident. Fugit ratione nostrum praesentium, doloremque qui nobis illo!',
+		price: 25.0,
+		image:
+      'https://images.unsplash.com/photo-1564277287253-934c868e54ea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bmF0dXJhbCUyMHJlbWVkeXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+		isActive: true,
+	},
+	{
+		id: uuidv4(),
+		productName: 'Lorem ipsum dolor sit amet.',
+		type: 'carousel',
+		category: 'Healty & Care',
+		description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores non quasi provident. Fugit ratione nostrum praesentium, doloremque qui nobis illo!',
+		price: 10.0,
+		image:
+      'https://images.unsplash.com/photo-1512867957657-38dbae50a35b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG5hdHVyYWwlMjByZW1lZHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+		isActive: true,
+	},
+	{
+		id: uuidv4(),
+		productName: 'Lorem ipsum dolor sit amet.',
+		type: 'default',
+		category: 'Healty & Care',
+		description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores non quasi provident. Fugit ratione nostrum praesentium, doloremque qui nobis illo!',
+		price: 5.0,
+		image:
+      'https://images.unsplash.com/photo-1512867957657-38dbae50a35b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG5hdHVyYWwlMjByZW1lZHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+		isActive: true,
+	},
+	{
+		id: uuidv4(),
+		productName: 'Lorem ipsum dolor sit amet.',
+		type: 'carousel',
+		category: 'Healty & Care',
+		description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores non quasi provident. Fugit ratione nostrum praesentium, doloremque qui nobis illo!',
+		price: 8.0,
+		image:
+      'https://images.unsplash.com/photo-1563483783225-fdcd3bf7fb68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzB8fG5hdHVyYWwlMjByZW1lZHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+		isActive: true,
+	},
+	{
+		id: uuidv4(),
+		productName: 'Lorem ipsum dolor sit amet.',
+		type: 'default',
+		category: 'Healty & Care',
+		description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores non quasi provident. Fugit ratione nostrum praesentium, doloremque qui nobis illo!',
+		price: 32.0,
+		image:
+      'https://images.unsplash.com/photo-1635943051866-3271c28168f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fG5hdHVyYWwlMjByZW1lZHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
 		isActive: false,
+	},
+	{
+		id: uuidv4(),
+		productName: 'Lorem ipsum dolor sit amet.',
+		type: 'carousel',
+		category: 'Healty & Care',
+		description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores non quasi provident. Fugit ratione nostrum praesentium, doloremque qui nobis illo!',
+		price: 32.0,
+		image:
+      'https://images.unsplash.com/photo-1630340867123-1ee66b32abe3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzF8fG5hdHVyYWwlMjByZW1lZHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+		isActive: false,
+	},
+	{
+		id: uuidv4(),
+		productName: 'Omega 3',
+		type: 'highlight',
+		category: 'food',
+		description:
+      'super suplement for your health and well-being natural and 100% functional, take advantage og this offer',
+		price: 10.5,
+		image:
+      'https://images.unsplash.com/photo-1633171031508-a8f26271e8aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+		isActive: true,
 	},
 ];
 
