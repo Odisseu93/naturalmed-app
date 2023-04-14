b0c9b38b99bcbc1f57dccd5dce09d41aa989f4b2
Ulisses <ulissessuporteti01@gmail.com>
2023-04-13 22:46:29 -0300

    feat: add fonts

---
+++ src/assets/fonts/index.ts
@@ -0,0 +1,9 @@
+import { Roboto } from 'next/font/google';
+const roboto400 = Roboto({weight: '400', subsets: ['latin']});
+const roboto500 = Roboto({weight: '500', subsets: ['latin']});
+const roboto700 = Roboto({weight: '700', subsets: ['latin']});
+
+
+
+
+export const fonts = { roboto400, roboto500, roboto700 };
