effab9f181ef67da7a9d3a60130c65f61048ec92
Ulisses <ulissessuporteti01@gmail.com>
2023-04-15 17:21:37 -0300

    refactor: css style

--- src/components/ButtonBuy/index.tsx
+++ src/components/ButtonBuy/index.tsx
@@ -6,7 +6,7 @@ type TProps = {
 }
 
 const ButtonBuy: React.FC<TProps> = ({text}:TProps)=> {
-	return <button className={`${styles.btn} ${fonts.roboto700.className} p-1 rounded-full mt-2`}>{text}</button>;
+	return <button className={`${styles.btn} ${fonts.roboto700.className}`}>{text}</button>;
 };
 
 export default ButtonBuy;
