0abc14dbe3a2c6828e4725d55742e7e9cf4644d3
Ulisses <ulissessuporteti01@gmail.com>
2023-04-17 18:30:43 -0300

    feat(_document.tsx): add title and favicon to the application

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head />
+			<Head>
+				<title>Naturalmed App</title>	
+				<link  rel="shortcut icon" href="./assets/favicon.svg" />
+			</ Head>
 			<body>
 				<Main />
 				<NextScript />
